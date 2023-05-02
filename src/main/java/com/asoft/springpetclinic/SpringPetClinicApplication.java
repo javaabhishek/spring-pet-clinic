7cb327993d06fba31628d240aac9caf85da67bdf
Patil <avpatil@elogex.com>
2023-05-02 08:31:55 +0530

    Test commit working

--- src/main/java/com/asoft/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/com/asoft/springpetclinic/SpringPetClinicApplication.java
@@ -8,6 +8,7 @@ public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPetClinicApplication.class, args);
+		System.out.println("Check commit");
 	}
 
 }
