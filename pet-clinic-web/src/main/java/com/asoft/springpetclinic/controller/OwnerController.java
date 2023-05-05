ff2034ae3ab1f71b55dd20b1bbb5001f295f71de
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-05 16:51:56 +0530

    Close issue #16

--- pet-clinic-web/src/main/java/com/asoft/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/asoft/springpetclinic/controller/OwnerController.java
@@ -3,10 +3,11 @@ package com.asoft.springpetclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"owner","owners","owners/index","owners/index"})
+    @RequestMapping({"/","/index","/index,html"})
     public String index(){
         return "owners/index";
     }
