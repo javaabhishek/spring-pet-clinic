e1f61815bba55b45f852e76e146cd9da7871bd98
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-05 16:37:26 +0530

    #12

--- pet-clinic-web/src/main/java/com/asoft/springpetclinic/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/asoft/springpetclinic/SpringPetClinicApplication.java
@@ -8,7 +8,6 @@ public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPetClinicApplication.class, args);
-		System.out.println("Check commit in master");
 	}
 
 }
