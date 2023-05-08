030c57b3fe8a2cc6ce779261e1984569faad42f1
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-08 10:05:59 +0530

    Resolved #10

--- pet-clinic-web/src/main/java/com/asoft/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/asoft/springpetclinic/controller/OwnerController.java
@@ -21,6 +21,7 @@ public class OwnerController {
     @RequestMapping({"","/","/index","/index,html"})
     public String index(Model model){
         Set<Owner> owners=ownerService.findAll();
+
         model.addAttribute("owners",owners);
         return "owners/index";
     }
