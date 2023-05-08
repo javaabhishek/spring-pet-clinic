2b51d1d51edbeaefff6d66ba61ba3c1d95bb349c
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-08 10:03:24 +0530

    Resolved #10

--- pet-clinic-web/src/main/java/com/asoft/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/asoft/springpetclinic/controller/OwnerController.java
@@ -1,14 +1,27 @@
 package com.asoft.springpetclinic.controller;
 
+import com.asoft.springpetclinic.model.Owner;
+import com.asoft.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index,html"})
-    public String index(){
+    public String index(Model model){
+        Set<Owner> owners=ownerService.findAll();
+        model.addAttribute("owners",owners);
         return "owners/index";
     }
 }
