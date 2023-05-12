5dde55e2717772a6cd2d40255075af9b2a6169e9
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-12 11:27:46 +0530

    Clean code- Removed @Service annotation from OwnerService Interface

--- pet-clinic-data/src/main/java/com/asoft/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/asoft/springpetclinic/services/OwnerService.java
@@ -3,7 +3,7 @@ package com.asoft.springpetclinic.services;
 import com.asoft.springpetclinic.model.Owner;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface OwnerService extends CrudBaseService<Owner,Long> {
     Owner findByLastName(String lastName);
 }
