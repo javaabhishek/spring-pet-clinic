d0dd0472250f6e0735920c7f8d5f4096b511f686
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-12 10:33:49 +0530

    Bootstrap: Add PetTypes data, closes #32

--- pet-clinic-web/src/main/java/com/asoft/springpetclinic/bootstrap/BootstrapDataLoader.java
+++ pet-clinic-web/src/main/java/com/asoft/springpetclinic/bootstrap/BootstrapDataLoader.java
@@ -1,8 +1,10 @@
 package com.asoft.springpetclinic.bootstrap;
 
 import com.asoft.springpetclinic.model.Owner;
+import com.asoft.springpetclinic.model.PetType;
 import com.asoft.springpetclinic.model.Vet;
 import com.asoft.springpetclinic.services.OwnerService;
+import com.asoft.springpetclinic.services.PetTypeService;
 import com.asoft.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class BootstrapDataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public BootstrapDataLoader(OwnerService ownerService, VetService vetService) {
+
+    public BootstrapDataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog=new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType=petTypeService.save(dog);
+
+        PetType cat=new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType=petTypeService.save(cat);
+
         Owner onr1=new Owner();
         onr1.setFirstName("Abhishek");
         onr1.setLastName("Patil");
@@ -43,5 +56,6 @@ public class BootstrapDataLoader implements CommandLineRunner {
 
         System.out.println(ownerService.findAll().size());
         System.out.println(vetService.findAll().size());
+        System.out.println(petTypeService.findAll().size());
      }
 }
