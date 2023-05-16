572aef2459d8775b43cc72fe277f73101b1f0452
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-16 09:09:19 +0530

    Create a Visit map service,Closes #47

---
+++ pet-clinic-data/src/main/java/com/asoft/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.asoft.springpetclinic.services;
+
+import com.asoft.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudBaseService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/asoft/springpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,38 @@
+package com.asoft.springpetclinic.services.map;
+
+import com.asoft.springpetclinic.model.Visit;
+import com.asoft.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit model) {
+        if(model.getPet()==null || model.getPet().getOwner()==null
+        || model.getPet().getId()==null || model.getPet().getOwner().getId()==null)
+            throw new RuntimeException("Invalid visit object");
+        return super.save(model);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long modelId) {
+        super.deleteById(modelId);
+    }
+
+    @Override
+    public void delete(Visit model) {
+        super.delete(model);
+    }
+}
